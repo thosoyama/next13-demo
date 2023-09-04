86dc5539df706d702906d605b51359ee82d9ebc8
t-hosoyama <terunobu-hosoyama@p3.techfirm.co.jp>
2023-09-04 15:28:34 +0900

    replace info area

--- src/features/reversi/components/parts/Disc.tsx
+++ src/features/reversi/components/parts/Disc.tsx
@@ -3,20 +3,24 @@ import type { Disc } from '../../context'
 type SquareProps = {
   item: Disc
   opacity?: boolean
+  children?: React.ReactNode
 }
 
 /**
  * 石
  */
-export function Disc({ item, opacity = false }: SquareProps) {
-  const color = item === 1 ? 'bg-black' : 'bg-white'
+export function Disc({ item, children = null, opacity = false }: SquareProps) {
+  const color = item === 1 ? 'text-gray-300' : 'text-gray-800'
+  const bgColor = item === 1 ? 'bg-black' : 'bg-white'
   const borderColor = item === 1 ? 'border-gray-500' : 'border-gray-600'
 
   return (
     <div
-      className={`absolute h-5/6 w-5/6 rounded-full border shadow-sm shadow-black ${color} ${borderColor} ${
+      className={`absolute flex h-5/6 w-5/6 items-center justify-center rounded-full border text-xs shadow-sm shadow-black ${color} ${bgColor} ${borderColor} ${
         opacity ? 'opacity-70' : 'opacity-100'
       }`}
-    />
+    >
+      {children}
+    </div>
   )
 }
--- src/features/reversi/components/parts/Info.tsx
+++ src/features/reversi/components/parts/Info.tsx
@@ -10,17 +10,33 @@ export function Info() {
 
   return (
     <>
-      <div className="relative flex flex-col items-center">
-        <div className="relative mx-5 box-border flex h-12 w-12 items-center justify-center border-2 border-black bg-green-700">
-          <Disc item={state.winner !== 0 ? state.winner : state.first ? 1 : -1} />
-        </div>
-        <div className="flex w-12 justify-center font-bold">
-          {state.winner !== 0 ? 'WIN' : state.pass ? 'PASS' : ''}
+      <div className="relative flex flex-col">
+        <div className="flex">
+          <div className="flex flex-col items-center">
+            <div className="relative ml-2 box-border flex h-12 w-12 items-center justify-center border-2 border-black bg-green-700">
+              <Disc item={1}>{state.count.first}</Disc>
+            </div>
+            <div className="ml-2 flex w-12 justify-center font-bold">
+              {state.first && state.winner === 1 ? 'WIN' : state.first && state.pass ? 'PASS' : ''}
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="relative ml-2 box-border flex h-12 w-12 items-center justify-center border-2 border-black bg-green-700">
+              <Disc item={-1}>{state.count.second}</Disc>
+            </div>
+            <div className="ml-2 flex  w-12 justify-center font-bold">
+              {!state.first && state.winner === -1
+                ? 'WIN'
+                : !state.first && state.pass
+                ? 'PASS'
+                : ''}
+            </div>
+          </div>
         </div>
         <button
           type="button"
           onClick={() => dispatch({ type: 'RESET' })}
-          className="absolute bottom-0 mt-2 rounded-sm border-2 border-black px-1 text-xs font-bold hover:bg-slate-200"
+          className="absolute bottom-0 ml-2 mt-2 rounded-sm border-2 border-black px-1 text-xs font-bold hover:bg-slate-200"
         >
           RESET
         </button>
